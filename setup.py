ecec3aba5035202d850c8920587fd5e8b81acfc5
Salma EL HAJJAMI <11726769+salmaelhajjami@users.noreply.github.com>
2023-04-28 14:05:56 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='osbnr',
+    version='0.1',
+    description='One Side Behavioral Noise Reduction algorithm implementation',
+    author='Salma EL HAJJAMI',
+    author_email='s.elhajjami@uiz.ac.ma',
+    packages=['osbnr'],
+    install_requires=['scikit-learn', 'numpy'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
